a3b00e9237377799f002f91dd9d638568a91edef
Shalevskaya Tanya <Bogdan0va1009@yandex.ru>
2022-04-28 22:16:54 +0300

    pw6

--- scripts/validate.js
+++ scripts/validate.js
@@ -56,8 +56,8 @@ function enableValidation(object) {
 enableValidation({
     formSelector: '.popup__form',
     inputSelector: '.popup__input',
-    inputErrorClass: 'form__input_type_error',
-    errorClass: 'form__input-error_active'
+    inputErrorClass: 'popup__input_type_error',
+    errorClass: 'popup__input-error_active'
 });
 
 
